f499f86c2fe36c95f5c0f20f62c08893af61917a
Waldemar Celes <celes@tecgraf.puc-rio.br>
1993-12-17 16:41:19 -0200

    io library

--- inout.h
+++ inout.h
@@ -1,9 +1,5 @@
 /*
-** inout.h
-**
-** Waldemar Celes Filho
-** TeCGraf - PUC-Rio
-** 11 May 93
+** $Id: $
 */
 
 
@@ -17,6 +13,7 @@ extern int lua_debugline;
 int  lua_openfile     (char *fn);
 void lua_closefile    (void);
 int  lua_openstring   (char *s);
+void lua_closestring  (void);
 int  lua_pushfunction (int file, int function);
 void lua_popfunction  (void);
 void lua_reportbug    (char *s);
