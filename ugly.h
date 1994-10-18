3386e3c1fba2a829fdf5987edce2a501d6379a5a
Waldemar Celes <celes@tecgraf.puc-rio.br>
1994-10-18 12:30:29 -0200

    definition of ugly tokens

---
+++ ugly.h
@@ -0,0 +1,24 @@
+enum
+{
+ U_and=128,
+ U_do,
+ U_else,
+ U_elseif,
+ U_end,
+ U_function,
+ U_if,
+ U_local,
+ U_nil,
+ U_not,
+ U_or,
+ U_repeat,
+ U_return,
+ U_then,
+ U_until,
+ U_while,
+ U_eq = '='+128,
+ U_le = '<'+128,
+ U_ge = '>'+128,
+ U_ne = '~'+128,
+ U_sc = '.'+128
+};
