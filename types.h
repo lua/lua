4dce79f7e30787bb21d96cd117217fa157e34594
Waldemar Celes <celes@tecgraf.puc-rio.br>
1994-12-27 18:41:47 -0200

    uses 'limits.h' for MAX_WORD and MAX_INT

--- types.h
+++ types.h
@@ -1,10 +1,12 @@
 /*
 ** TeCGraf - PUC-Rio
-** $Id: $
+** $Id: types.h,v 1.1 1994/12/20 21:31:01 roberto Exp celes $
 */
 
 #ifndef types_h
 #define types_h
+
+#include <limits.h>
 
 #ifndef real
 #define real float
@@ -16,7 +18,8 @@ typedef unsigned char  Byte;  /* unsigned 8 bits */
 
 typedef unsigned short Word;  /* unsigned 16 bits */
 
-#define MAX_WORD  0xFFFD  /* maximum value of a word (FFFF-2 for safety) */
+#define MAX_WORD  (USHRT_MAX-2)  /* maximum value of a word (-2 for safety) */
+#define MAX_INT   (INT_MAX-2)  /* maximum value of a int (-2 for safety) */
 
 typedef signed long  Long;  /* 32 bits */
 
