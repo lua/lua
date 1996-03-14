c2eb02aaf6f0c852b5528a6d47d098af103a6258
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
1996-03-14 12:53:09 -0300

    both "write" and "format" need "addquoted".

--- lualib.h
+++ lualib.h
@@ -2,7 +2,7 @@
 ** Libraries to be used in LUA programs
 ** Grupo de Tecnologia em Computacao Grafica
 ** TeCGraf - PUC-Rio
-** $Id: lualib.h,v 1.5 1996/01/22 17:38:57 roberto Exp roberto $
+** $Id: lualib.h,v 1.6 1996/02/09 19:00:23 roberto Exp roberto $
 */
 
 #ifndef lualib_h
@@ -18,6 +18,7 @@ void lua_arg_error(char *funcname);
 char *lua_check_string (int numArg, char *funcname);
 double lua_check_number (int numArg, char *funcname);
 char *luaI_addchar (int c);
+void luaI_addquoted (char *s);
 
 #endif
 
