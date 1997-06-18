b90b4bbd3aaa287b53663d0892d0ff85fe04b812
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
1997-06-18 18:39:56 -0300

    macros don't need prototypes.

--- zio.h
+++ zio.h
@@ -1,7 +1,7 @@
 /*
 * zio.h
 * a generic input stream interface
-* $Id: zio.h,v 1.1 1997/06/16 16:50:22 roberto Exp roberto $
+* $Id: zio.h,v 1.2 1997/06/18 20:30:52 roberto Exp roberto $
 */
 
 #ifndef zio_h
@@ -17,21 +17,19 @@
 #define zpopen	luaz_popen
 #define zsopen	luaz_sopen
 #define zmopen	luaz_mopen
+#define zread	luaz_read
 
 #define EOZ	(-1)			/* end of stream */
 
 typedef struct zio ZIO;
-
-int zgetc(ZIO* z);			/* get next byte */
-int zungetc(ZIO* z);			/* put back last byte read */
-int zread(ZIO* z, void* b, int n);	/* read next n bytes */
-int zclose(ZIO* z);			/* close stream */
 
 ZIO* zFopen(ZIO* z, FILE* f);		/* open FILEs */
 ZIO* zfopen(ZIO* z, char* s, char* m);	/* file by name */
 ZIO* zpopen(ZIO* z, char* s, char* m);	/* pipe */
 ZIO* zsopen(ZIO* z, char* s);		/* string */
 ZIO* zmopen(ZIO* z, char* b, int size);	/* memory */
+
+int zread(ZIO* z, void* b, int n);	/* read next n bytes */
 
 #define zgetc(z)	(--(z)->n>=0 ? ((int)*(z)->p++): (z)->filbuf(z))
 #define zungetc(z)	(++(z)->n,--(z)->p)
