3a89c973ff139c7048ee9d85e347f4b054f62b89
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
1997-10-16 19:14:47 -0200

    details

--- lopcodes.h
+++ lopcodes.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lopcodes.h,v 1.8 1997/10/13 22:12:04 roberto Exp roberto $
+** $Id: lopcodes.h,v 1.9 1997/10/16 10:59:34 roberto Exp roberto $
 ** Opcodes for Lua virtual machine
 ** See Copyright Notice in lua.h
 */
@@ -135,9 +135,9 @@ IFTUPJMPW,/*	w	x		-		(x!=nil)? PC-=w  */
 IFFUPJMP,/*	b	x		-		(x==nil)? PC-=b  */
 IFFUPJMPW,/*	w	x		-		(x==nil)? PC-=w  */
 
-CLOSURE,/*	b	v_b...v_1 prt	c(prt) */
-CLOSURE0,/*	b	prt		c(prt) */
-CLOSURE1,/*	b	v_1 prt		c(prt) */
+CLOSURE,/*	b	v_b...v_1 proto	c(proto) */
+CLOSURE0,/*	-	proto		c(proto) */
+CLOSURE1,/*	-	v_1 proto	c(proto) */
 
 CALLFUNC,/*	b c	v_c...v_1 f	r_b...r_1	f(v1,...,v_c)  */
 CALLFUNC0,/*	b	v_b...v_1 f	-		f(v1,...,v_b)  */
