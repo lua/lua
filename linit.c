fb55c967062b1b13bac298a8f3bcb7ea0663a19c
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2000-08-09 11:50:13 -0300

    obsolete module; not part of the oficial distribution

--- linit.c
+++ linit.c
@@ -1,20 +1,8 @@
 /*
-** $Id: linit.c,v 1.4 2000/06/12 13:52:05 roberto Exp roberto $
+** $Id: linit.c,v 1.5 2000/06/16 17:22:43 roberto Exp roberto $
 ** Initialization of libraries for lua.c
 ** See Copyright Notice in lua.h
 */
 
-#include "lua.h"
-
-#include "lualib.h"
-
-
-void lua_userinit (void) {
-  lua_beginblock();
-  lua_iolibopen();
-  lua_strlibopen();
-  lua_mathlibopen();
-  lua_dblibopen();
-  lua_endblock();
-}
+>>>>>>> This module is now obsolete, and is not part of Lua. <<<<<<<<<
 
