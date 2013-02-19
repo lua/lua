b1e682aa2f5a272036f823e7861a6e973a976636
Lua Team <team@lua.org>
2013-02-19 12:00:00 +0000

    Lua 5.2.2-rc1

--- src/lvm.c
+++ src/lvm.c
@@ -1,5 +1,5 @@
 /*
-** $Id: lvm.c,v 2.152 2012/06/08 15:14:04 roberto Exp $
+** $Id: lvm.c,v 2.154 2012/08/16 17:34:28 roberto Exp $
 ** Lua virtual machine
 ** See Copyright Notice in lua.h
 */
@@ -98,7 +98,6 @@ static void callTM (lua_State *L, const TValue *f, const TValue *p1,
   setobj2s(L, L->top++, p2);  /* 2nd argument */
   if (!hasres)  /* no result? 'p3' is third argument */
     setobj2s(L, L->top++, p3);  /* 3rd argument */
-  luaD_checkstack(L, 0);
   /* metamethod may yield only when called from Lua code */
   luaD_call(L, L->top - (4 - hasres), hasres, isLua(L->ci));
   if (hasres) {  /* if has result, move it to its place */
@@ -470,7 +469,7 @@ void luaV_finishOp (lua_State *L) {
         L->top = ci->top;  /* adjust results */
       break;
     }
-    case OP_TAILCALL: case OP_SETTABUP:  case OP_SETTABLE:
+    case OP_TAILCALL: case OP_SETTABUP: case OP_SETTABLE:
       break;
     default: lua_assert(0);
   }
