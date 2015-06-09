cbef15f3ea81a925abdcc11bf5870f3f0f1ce1cb
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2015-06-09 11:21:13 -0300

    comment (FALLTHROUGH position)

--- ltable.c
+++ ltable.c
@@ -1,5 +1,5 @@
 /*
-** $Id: ltable.c,v 2.109 2015/04/02 21:09:51 roberto Exp roberto $
+** $Id: ltable.c,v 2.110 2015/05/20 16:22:30 roberto Exp roberto $
 ** Lua tables (hash)
 ** See Copyright Notice in lua.h
 */
@@ -551,8 +551,8 @@ const TValue *luaH_get (Table *t, const TValue *key) {
       lua_Integer k;
       if (luaV_tointeger(key, &k, 0)) /* index is int? */
         return luaH_getint(t, k);  /* use specialized version */
-      /* else *//* FALLTHROUGH */
-    }
+      /* else... */
+    }  /* FALLTHROUGH */
     default: {
       Node *n = mainposition(t, key);
       for (;;) {  /* check whether 'key' is somewhere in the chain */
