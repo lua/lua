0d1c6f4188fe278d505f801d3bb8f8fd4749c8c4
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2016-07-19 14:12:21 -0300

    MAXINDEXRK can be redefined (for debugging, to force most values to go through registers)

--- lopcodes.h
+++ lopcodes.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lopcodes.h,v 1.147 2014/10/20 18:29:55 roberto Exp roberto $
+** $Id: lopcodes.h,v 1.148 2014/10/25 11:50:46 roberto Exp roberto $
 ** Opcodes for Lua virtual machine
 ** See Copyright Notice in lua.h
 */
@@ -139,7 +139,9 @@ enum OpMode {iABC, iABx, iAsBx, iAx};  /* basic instruction format */
 /* gets the index of the constant */
 #define INDEXK(r)	((int)(r) & ~BITRK)
 
+#if !defined(MAXINDEXRK)  /* (for debugging only) */
 #define MAXINDEXRK	(BITRK - 1)
+#endif
 
 /* code a constant index as a RK value */
 #define RKASK(x)	((x) | BITRK)
