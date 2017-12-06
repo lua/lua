338e1678931115ddbb1cbee0d42d25f502a914b9
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2017-12-06 16:36:31 -0200

    avoid using one function for different tasks (malloc, free, etc.)

--- lmem.c
+++ lmem.c
@@ -1,5 +1,5 @@
 /*
-** $Id: lmem.c,v 1.90 2015/03/03 18:18:29 roberto Exp roberto $
+** $Id: lmem.c,v 1.91 2015/03/06 19:45:54 roberto Exp roberto $
 ** Interface to Memory Manager
 ** See Copyright Notice in lua.h
 */
@@ -20,6 +20,14 @@
 #include "lmem.h"
 #include "lobject.h"
 #include "lstate.h"
+
+
+#if defined(HARDMEMTESTS)
+#define hardtest(L,os,s)  /* force a GC whenever possible */ \
+  if ((s) > (os) && (G(L))->gcrunning) luaC_fullgc(L, 1);
+#else
+#define hardtest(L,os,s)  ((void)0)
+#endif
 
 
 
@@ -45,10 +53,12 @@
 #define MINSIZEARRAY	4
 
 
-void *luaM_growaux_ (lua_State *L, void *block, int *size, size_t size_elems,
-                     int limit, const char *what) {
+void *luaM_growaux_ (lua_State *L, void *block, int nelems, int *size,
+                     int size_elems, int limit, const char *what) {
   void *newblock;
   int newsize;
+  if (nelems + 1 <= *size)  /* does one extra element still fit? */
+    return block;  /* nothing to be done */
   if (*size >= limit/2) {  /* cannot double it? */
     if (*size >= limit)  /* cannot grow even a little? */
       luaG_runerror(L, "too many %s (limit is %d)", what, limit);
@@ -65,8 +75,37 @@ void *luaM_growaux_ (lua_State *L, void *block, int *size, size_t size_elems,
 }
 
 
+void *luaM_shrinkvector_ (lua_State *L, void *block, int *size,
+                          int final_n, int size_elem) {
+  global_State *g = G(L);
+  void *newblock;
+  size_t oldsize = cast(size_t, (*size) * size_elem);
+  size_t newsize = cast(size_t, final_n * size_elem);
+  lua_assert(newsize <= oldsize);
+  newblock = (*g->frealloc)(g->ud, block, oldsize, newsize);
+  if (newblock == NULL && final_n > 0)  /* allocation failed? */
+    return block;  /* keep old block */
+  else {
+    g->GCdebt += newsize - oldsize;
+    *size = final_n;
+    return newblock;
+  }
+}
+
+
 l_noret luaM_toobig (lua_State *L) {
   luaG_runerror(L, "memory allocation error: block too big");
+}
+
+
+/*
+** Free memory
+*/
+void luaM_free_ (lua_State *L, void *block, size_t osize) {
+  global_State *g = G(L);
+  lua_assert((block == 0) == (block == NULL));
+  (*g->frealloc)(g->ud, block, osize, 0);
+  g->GCdebt -= osize;
 }
 
 
@@ -77,15 +116,11 @@ l_noret luaM_toobig (lua_State *L) {
 void *luaM_realloc_ (lua_State *L, void *block, size_t osize, size_t nsize) {
   void *newblock;
   global_State *g = G(L);
-  size_t realosize = (block) ? osize : 0;
-  lua_assert((realosize == 0) == (block == NULL));
-#if defined(HARDMEMTESTS)
-  if (nsize > realosize && g->gcrunning)
-    luaC_fullgc(L, 1);  /* force a GC whenever possible */
-#endif
+  lua_assert((osize == 0) == (block == NULL));
+  hardtest(L, osize, nsize);
   newblock = (*g->frealloc)(g->ud, block, osize, nsize);
   if (newblock == NULL && nsize > 0) {
-    lua_assert(nsize > realosize);  /* cannot fail when shrinking a block */
+    lua_assert(nsize > osize);  /* cannot fail when shrinking a block */
     if (g->version) {  /* is state fully built? */
       luaC_fullgc(L, 1);  /* try to free some memory... */
       newblock = (*g->frealloc)(g->ud, block, osize, nsize);  /* try again */
@@ -94,7 +129,27 @@ void *luaM_realloc_ (lua_State *L, void *block, size_t osize, size_t nsize) {
       luaD_throw(L, LUA_ERRMEM);
   }
   lua_assert((nsize == 0) == (newblock == NULL));
-  g->GCdebt = (g->GCdebt + nsize) - realosize;
+  g->GCdebt = (g->GCdebt + nsize) - osize;
   return newblock;
 }
 
+
+void *luaM_malloc (lua_State *L, size_t size, int tag) {
+  hardtest(L, 0, size);
+  if (size == 0)
+    return NULL;  /* that's all */
+  else {
+    global_State *g = G(L);
+    void *newblock = (*g->frealloc)(g->ud, NULL, tag, size);
+    if (newblock == NULL) {
+      if (g->version) {  /* is state fully built? */
+        luaC_fullgc(L, 1);  /* try to free some memory... */
+        newblock = (*g->frealloc)(g->ud, NULL, tag, size);  /* try again */
+      }
+      if (newblock == NULL)
+        luaD_throw(L, LUA_ERRMEM);
+    }
+    g->GCdebt += size;
+    return newblock;
+  }
+}
