ea1322ef5438aa38f16fa00d3ab377811bba5a76
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2018-10-23 12:58:38 -0300

    Detail: bad assertion in 'luaM_free_'

--- lmem.c
+++ lmem.c
@@ -108,7 +108,7 @@ l_noret luaM_toobig (lua_State *L) {
 */
 void luaM_free_ (lua_State *L, void *block, size_t osize) {
   global_State *g = G(L);
-  lua_assert((block == 0) == (block == NULL));
+  lua_assert((osize == 0) == (block == NULL));
   (*g->frealloc)(g->ud, block, osize, 0);
   g->GCdebt -= osize;
 }
