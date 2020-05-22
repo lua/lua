efcf24be0c22cba57b298161bf4ab0561fd3c08e
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2020-05-22 15:39:29 -0300

    'luaL_execresult' does not assume -1 status as error
    ISO C is silent about the return of 'system'. Windows sets 'errno' in
    case of errors. Linux has several different error cases, with different
    return values. ISO C allows 'system' to set 'errno' even if there are no
    errors. Here we assume that a status==0 is success (which is the case
    on several platforms), otherwise it is an error. If there is an error
    number, gives the error based on it. (The worst a spurious 'errno'
    can do is to generate a bad error message.) Otherwise uses the normal
    results.

--- lauxlib.c
+++ lauxlib.c
@@ -284,7 +284,7 @@ LUALIB_API int luaL_fileresult (lua_State *L, int stat, const char *fname) {
 
 LUALIB_API int luaL_execresult (lua_State *L, int stat) {
   const char *what = "exit";  /* type of termination */
-  if (stat == -1)  /* error? */
+  if (stat != 0 && errno != 0)  /* error with an 'errno'? */
     return luaL_fileresult(L, 0, NULL);
   else {
     l_inspectstat(stat, what);  /* interpret result */
