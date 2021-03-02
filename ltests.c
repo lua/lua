9a2de786de5da862404995ddd3408f7ad3d54ee8
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2021-03-02 11:35:40 -0300

    Stack check in warning function for tests
    The warning function using for tests need to check the stack before
    pushing anything. (Warning functions are not expected to access a
    Lua state, therefore they have no preallocated stack space.)

--- ltests.c
+++ ltests.c
@@ -121,6 +121,7 @@ static void warnf (void *ud, const char *msg, int tocont) {
   strcat(buff, msg);  /* add new message to current warning */
   if (!tocont) {  /* message finished? */
     lua_unlock(L);
+    luaL_checkstack(L, 1, "warn stack space");
     lua_getglobal(L, "_WARN");
     if (!lua_toboolean(L, -1))
       lua_pop(L, 1);  /* ok, no previous unexpected warning */
@@ -142,6 +143,7 @@ static void warnf (void *ud, const char *msg, int tocont) {
       }
       case 2: {  /* store */
         lua_unlock(L);
+        luaL_checkstack(L, 1, "warn stack space");
         lua_pushstring(L, buff);
         lua_setglobal(L, "_WARN");  /* assign message to global '_WARN' */
         lua_lock(L);
