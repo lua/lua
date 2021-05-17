037aa8bd0741c66ef886f12170987a94aaeaa602
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-16 18:29:33 -0700

    don't auto load os lib

--- linit.c
+++ linit.c
@@ -45,7 +45,8 @@ static const luaL_Reg loadedlibs[] = {
   {LUA_COLIBNAME, luaopen_coroutine},
   {LUA_TABLIBNAME, luaopen_table},
   {LUA_IOLIBNAME, luaopen_io},
-  {LUA_OSLIBNAME, luaopen_os},
+// RUSEFI: don't auto load OS lib
+//  {LUA_OSLIBNAME, luaopen_os},
   {LUA_STRLIBNAME, luaopen_string},
   {LUA_MATHLIBNAME, luaopen_math},
   {LUA_UTF8LIBNAME, luaopen_utf8},
