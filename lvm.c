62fb93442753cbfb828335cd172e71471dffd536
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2021-07-22 13:44:53 -0300

    Bug: Negation in 'luaV_shiftr' may overflow
    Negation of an unchecked lua_Integer overflows with mininteger.

--- lvm.c
+++ lvm.c
@@ -766,7 +766,7 @@ lua_Number luaV_modf (lua_State *L, lua_Number m, lua_Number n) {
 /*
 ** Shift left operation. (Shift right just negates 'y'.)
 */
-#define luaV_shiftr(x,y)	luaV_shiftl(x,-(y))
+#define luaV_shiftr(x,y)	luaV_shiftl(x,intop(-, 0, y))
 
 lua_Integer luaV_shiftl (lua_Integer x, lua_Integer y) {
   if (y < 0) {  /* shift right? */
