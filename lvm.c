42d40581dd919fb134c07027ca1ce0844c670daf
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2022-05-20 13:14:33 -0300

    Save stack space while handling errors
    Because error handling (luaG_errormsg) uses slots from EXTRA_STACK,
    and some errors can recur (e.g., string overflow while creating an
    error message in 'luaG_runerror', or a C-stack overflow before calling
    the message handler), the code should use stack slots with parsimony.
    
    This commit fixes the bug "Lua-stack overflow when C stack overflows
    while handling an error".

--- lvm.c
+++ lvm.c
@@ -656,8 +656,10 @@ void luaV_concat (lua_State *L, int total) {
       /* collect total length and number of strings */
       for (n = 1; n < total && tostring(L, s2v(top - n - 1)); n++) {
         size_t l = vslen(s2v(top - n - 1));
-        if (l_unlikely(l >= (MAX_SIZE/sizeof(char)) - tl))
+        if (l_unlikely(l >= (MAX_SIZE/sizeof(char)) - tl)) {
+          L->top = top - total;  /* pop strings to avoid wasting stack */
           luaG_runerror(L, "string length overflow");
+        }
         tl += l;
       }
       if (tl <= LUAI_MAXSHORTLEN) {  /* is result a short string? */
@@ -672,7 +674,7 @@ void luaV_concat (lua_State *L, int total) {
       setsvalue2s(L, top - n, ts);  /* create result */
     }
     total -= n-1;  /* got 'n' strings to create 1 new */
-    L->top -= n-1;  /* popped 'n' strings and pushed one */
+    L->top = top - (n - 1);  /* popped 'n' strings and pushed one */
   } while (total > 1);  /* repeat until only 1 result left */
 }
 
