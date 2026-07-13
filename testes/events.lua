0f781f836ab348716eb9232d3831a871b9821a3c
Roberto I <roberto@inf.puc-rio.br>
2026-07-13 15:55:36 -0300

    Bug: Issues with write barrier for __newindex
    In 'luaV_finishset', there is an update on a table that is a field on
    another table. If the first table is the same as the one with the field
    (e.g., after 't.__newindex = t'), the update can change the value on
    that field (if the field being updated is '__newindex' itself). After
    that, the barrier is called with the table stored in that field, which
    is not the correct table anymore.

--- testes/events.lua
+++ testes/events.lua
@@ -382,6 +382,16 @@ do
 end
 
 
+do   -- bug in 5.4
+  local parent = {}
+  parent.__newindex = parent
+  collectgarbage()
+  local child = setmetatable({}, parent)
+  child.__newindex = {x = "hello"}
+  collectgarbage("step")
+  assert(parent.__newindex.x == "hello")
+end
+
 
 -- concat metamethod x numbers (bug in 5.1.1)
 c = {}
